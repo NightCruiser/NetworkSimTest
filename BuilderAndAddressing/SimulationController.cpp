c717b305569111f36c56604dc151fc7cfdf1db8c
Valentin <valentin.kriukov@aalto.fi>
2020-11-07 08:25:43 +0200

    upd

--- BuilderAndAddressing/SimulationController.cpp
+++ BuilderAndAddressing/SimulationController.cpp
@@ -47,7 +47,7 @@ bool SimulationController::BuildNetwork(std::string test) { /*Just FOR TEST 15 c
         pool_->AddNode(std::make_pair(tempNode, (uint32_t)321)); /*adding to pool*/
         /*CreateNode(nodes_ node, std::string name, uint32_t mac, std::pair<double,double> location, std::shared_ptr<AddressPool> pool)*/
         
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
                 std::cout << "Creating Node " << (i + 1) << std::endl; /*DELETE*/
                 std::string name = "Client " + std::to_string(i); /*DELETE*/
                 std::shared_ptr<Node> tmp = Creator::CreateNode(client, name, i, std::make_pair(15,52), pool_);
